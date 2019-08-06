3788ae339b2745b58f820b52eca0f6060da1cf3d
i7-8700 <1281261856@qq.com>
2019-08-06 23:12:52 +0800

    daily advice

---
+++ DailyAdvice/DailyAdviceClient.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ * DailyAdviceClient
+ */
+public class DailyAdviceClient {
+    public void go() {
+        try {
+            Socket socket = new Socket("127.0.0.1", 4242);
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+            String advice = reader.readLine();
+            System.out.println("Today you sould: " + advice);
+            reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        DailyAdviceClient client = new DailyAdviceClient();
+        client.go();
+    }
+}
